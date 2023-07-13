b0bcc38a16df42fa3fe313372f4414c3fc93d354
IBRA956 <zibrahimsalih@gmail.com>
2023-07-13 08:19:18 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf("Sise of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Sise of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Sise of an loat:: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Sise of an float:: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
