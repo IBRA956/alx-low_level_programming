9177e880485db42b2f004241dc14c93dcb428e55
IBRA956 <zibrahimsalih@gmail.com>
2023-07-14 05:40:30 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
-
 /**
- * main - prints all possible different combinations of two digits
- * Return: 0
+ * main - prints all the numbers of base 16
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, m;
+	int n;
+	char ch;
 
-	for (n = 48; n <= 56; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 49; m <= 57; m++)
-		{
-			if (m > n)
-			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
