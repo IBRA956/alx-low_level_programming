77f722d5300c3d4ef61a65027530dd8b35c7b477
IBRA956 <zibrahimsalih@gmail.com>
2023-07-18 09:31:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ *print_alphabet - print alphapet
+ *
+ *return: 0
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
