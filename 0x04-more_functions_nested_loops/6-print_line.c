eb00f7f9b66b5f0145bf6b1734bfc063ba29ec4d
IBRA956 <zibrahimsalih@gmail.com>
2023-07-20 06:02:02 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,19 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i;
+    if (n <= 0)
+    {
+        _putchar('\n'); // Assuming _putchar is a function to print a character
+    }
+    else
+    {
+        int i;
 
-		for (i = 1; i <= n; i++)
-		{
-			putchar('');
-		}
-		_putchar('\n');
-	}
-
+        for (i = 1; i <= n; i++)
+        {
+            _putchar('_'); // Print underscores ('_') n times
+        }
+        _putchar('\n'); // Move to the next line after printing the line of underscores
+    }
 }
+
