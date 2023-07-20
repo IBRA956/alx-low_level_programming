0455be268dda61be4fb464caf2a46b674c6f01d2
IBRA956 <zibrahimsalih@gmail.com>
2023-07-20 06:15:57 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-main.c
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -5,12 +5,12 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-    	 print_line(0);
-   	 print_line(2);
-   	 print_line(10);
-   	 print_line(-4);
-   	 return (0);
+    print_line(0);    // Will print an empty line because n is 0.
+    print_line(2);    // Will print "__" (two underscores) in a new line.
+    print_line(10);   // Will print "__________" (ten underscores) in a new line.
+    print_line(-4);   // Will print an empty line because n is less than or equal to 0.
+    return (0);
 }
+
